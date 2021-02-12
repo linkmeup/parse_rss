540cfb4b2652cff0be848707ce6f389d5a5e4587
eucariot <eucariot@gmail.com>
2021-02-12 23:40:28 +0700

    Add prints

--- get_podcasts.py
+++ get_podcasts.py
@@ -17,45 +17,45 @@ def get_podcast_kdpv(description):
     match = re.search(regexp, description)
 
     if match:
-        if not "patreon" in match.group("img"):
-            return match.group("img")
+        if not 'patreon' in match.group('img'):
+            return match.group('img')
 
 
 def get_podcasts(rss):
     all_podcasts = []
-    for item in rss["channel"]:
-        if item  == "item":
-            for podcast in rss["channel"][item]:
-                short_description = ""
+    for item in rss['channel']:
+        if item  == 'item':
+            for podcast in rss['channel'][item]:
+                short_description = ''
                 this_podcast = {}
                 
                 for head in podcast:
-                    if head == "title":
-                        this_podcast.update({"title": podcast[head]})
-                    elif head == "link":
+                    if head == 'title':
+                        this_podcast.update({'title': podcast[head]})
+                    elif head == 'link':
                         link = podcast[head]
-                        this_podcast.update({"link": link})
-                    elif head == "enclosure":
+                        this_podcast.update({'link': link})
+                    elif head == 'enclosure':
                         for thing in podcast[head]:
-                            if thing == "@url":
-                                this_podcast.update({"podcast_url": podcast[head][thing]})
-                    elif head == "itunes:summary":
+                            if thing == '@url':
+                                this_podcast.update({'podcast_url': podcast[head][thing]})
+                    elif head == 'itunes:summary':
                         short_description = podcast[head]
-                    elif head == "description":
+                    elif head == 'description':
                         img = get_podcast_kdpv(podcast[head])
                         if img:
-                            this_podcast.update({"kdpv": img})
+                            this_podcast.update({'kdpv': img})
                         description = podcast[head]
 
                 if len(short_description) > 20:
-                    body = f"{short_description}\n\n{link}"
+                    body = f'{short_description}\n\n{link}'
                 else:
                     f = HTMLFilter()
                     f.feed(description)
                     
-                    body = f"{f.text}\n\n{link}"
+                    body = f'{f.text}\n\n{link}'
 
-                this_podcast.update({"body": body})
+                this_podcast.update({'body': body})
 
                 all_podcasts.append(this_podcast)
 
@@ -63,13 +63,16 @@ def get_podcasts(rss):
 
 
 def main():
-    r = requests.get("https://linkmeup.ru/rss/podcasts", verify=False)
-    rss = xmltodict.parse(r.text)["rss"]
+    print('Start RSS parsing...')
+    r = requests.get('https://linkmeup.ru/rss/podcasts')
+    rss = xmltodict.parse(r.text)['rss']
 
     all_podcasts = get_podcasts(rss)
 
     with open('all_podcasts.json', 'w') as f:
         json.dump(all_podcasts, f)
+
+    print('Dict saved to all_podcasts.json.')
 
 
 if __name__ == '__main__':
